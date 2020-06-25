8c317f92f9bc543a716cca150bb89de9aaf985ef
oshimamasara <oshimamasara@gmail.com>
2020-06-25 14:37:28 +0900

    pay function OK

--- pay/urls.py
+++ pay/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payjp, name='payjp'),
+    path('payjp-form', views.payjp, name='payjp'), # payjp 公式 doc
+    path('payjp2', views.payjp_2, name='payjp2'), 
 ]
--- pay/views.py
+++ pay/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-user_id = 100
-
-
 def payjp(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     if request.method == "POST":
@@ -13,10 +10,6 @@ def payjp(request):
         user_name = request.POST['input_name']
         user_email = request.POST['input_mail']
         user_address = request.POST['input_address']
-        card_number = request.POST['card_number']
-        card_cvc = request.POST['card_cvc']
-        card_month = request.POST['card_month']
-        card_year = request.POST['card_year']
 
         # pay.jp token from form method
         print(request.POST['payjp-token'])
@@ -39,3 +32,59 @@ def payjp(request):
         return render(request, 'pay/thanks.html', context)
 
     return render(request, 'pay/payjp.html')
+
+def payjp_2(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "POST":
+        item_name = "うまい！おせんべい" # 本当は Database から
+        item_price = 100 # 本当は Database から
+        user_name = request.POST['input_name']
+        user_email = request.POST['input_mail']
+        user_address = request.POST['input_address']
+        card_number = request.POST['card_number']
+        card_cvc = request.POST['card_cvc']
+        card_month = request.POST['card_month']
+        card_year = request.POST['card_year']
+
+        # token
+        import payjp # この位置じゃないとダメ
+        payjp.api_key = 'sk_test_9e835c1a09250c18e4891c1d'
+        token = payjp.Token.create(
+            card={
+                'number' : card_number,
+                'cvc' : card_cvc,
+                'exp_month' : card_month,
+                'exp_year' : card_year
+            },
+            headers={'X-Payjp-Direct-Token-Generate': 'true'}
+        )
+        token_id = token['id']
+        print('\n+++++ token')
+        print(token_id)
+
+        # create user
+        user =payjp.Customer.create(
+            #name = user_name, # 決済フローの中で name は指定できない様子
+            description= "どこから？　" + item_name,
+            email = user_email,
+            card = token_id
+        )
+        user_id = user["id"]
+        print('\n---- create user')
+        print(user_id)
+
+        ## pay
+        item_price = int(item_price)
+        charge = payjp.Charge.create(
+            amount= item_price,
+            customer=user_id,
+            currency="jpy",
+        )
+        paied_id = charge['id']
+        print('\n$$$$ charge')
+        print(charge)
+
+        context = {'item_name':item_name, 'item_price':item_price ,'name':user_name, 'email':user_email, 'address':user_address, 'paied_id':paied_id}
+        return render(request, 'pay/thanks2.html', context)
+    
+    return render(request, 'pay/payjp2.html')

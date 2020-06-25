5ec1e24f9f51436df5c9c01fe5c01858ec4df30a
oshimamasara <oshimamasara@gmail.com>
2020-06-25 13:09:47 +0900

    payjp error....

--- pay/views.py
+++ pay/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import payjp
+payjp.api_key = 'sk_test_9e835c1a09250c18e4891c1d'
+
+user_id = 100
 
 
 def payjp(request):
@@ -14,7 +18,7 @@ def payjp(request):
         card_cvc = request.POST['card_cvc']
         card_month = request.POST['card_month']
         card_year = request.POST['card_year']
-
+        
         print(user_name)
         print(user_email)
         print(user_address)
@@ -22,6 +26,44 @@ def payjp(request):
         print(card_cvc)
         print(card_month)
         print(card_year)
+
+        #try:
+            # create token
+        token = payjp.Token.create(
+            card={
+                'number' : card_number,
+                'cvc' : card_cvc,
+                'exp_month' : card_month,
+                'exp_year' : card_year
+            },
+            headers={'X-Payjp-Direct-Token-Generate': 'true'}
+        )
+        token_id = token['id']
+        print('+++++ token')
+        print(token_id)
+        # create user
+        user_id = str(user_id + 1)
+        user =payjp.Customer.create(
+            description= item_name,
+            id = user_id,
+            email = user_email,
+            card = token_id
+        )
+        user_id = user["id"]
+        print('---- create user')
+        print(user_id)
+        ## pay
+        item_price = int(item_price)
+        charge = payjp.Charge.create(
+            amount= item_price,
+            customer=user_id,
+            currency="jpy",
+        )
+        print('$$$$ charge')
+        print(charge)
+        #except Exception as e:
+        #    print("例外args:", e.args)
+
         context = {'item_name':item_name, 'item_price':item_price ,'name':user_name, 'email':user_email, 'address':user_address}
         return render(request, 'pay/thanks.html', context)
         #return HttpResponse("Hello, world. You're at the polls index.")
